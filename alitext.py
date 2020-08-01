aebfe15ce2c94efba40b16013e48017972f91592
oliverkolbet <oliver.kolbet@gmail.com>
2020-08-01 15:08:12 -0700

    modified:   alitext.py 	new file:   rawpacks/xhred.aliraw

--- alitext.py
+++ alitext.py
@@ -1,3 +1,12 @@
 import os
 from time import sleep
 import pyperclip
+tmp_file=open('.tmp_atxt','r')
+tmp_info=tmp_file.read()
+tmp_info.split('\n').strip('\n')
+ali_file_name=tmp_info[0]
+if len(tmp_info) > 1:
+	file_seg=tmp_info[1]
+	if len(tmp_info) > 2:
+		file_args=tmp_info[2]
+		file_args.split(':')
