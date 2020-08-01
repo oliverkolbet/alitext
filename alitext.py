ef1062ec969583e9af1ebf7baac692a61afdd57a
oliverkolbet <oliver.kolbet@gmail.com>
2020-08-01 15:18:07 -0700

    modified:   alitext.py

--- alitext.py
+++ alitext.py
@@ -10,3 +10,15 @@ if len(tmp_info) > 1:
 	if len(tmp_info) > 2:
 		file_args=tmp_info[2]
 		file_args.split(':')
+ftype = (ali_file_name.split('.'))
+if ftype[1] == 'aliraw':
+	file=open(ftype[0],'r')
+	file=file.read().strip().split('\n')
+	title=file[0]
+	descrip=file[2]
+	descrip=descrip[1:]
+	print(descrip)
+	sleep(4)
+	if title == '<ALIAS>':
+	elif title == '<COPY>':
+	elif title == '<RUN>':
