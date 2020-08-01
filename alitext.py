e02f4b86f7112decdd0bcab1d650abe22dd29b0d
oliverkolbet <oliver.kolbet@gmail.com>
2020-08-01 15:47:06 -0700

    modified:   alitext.py

--- alitext.py
+++ alitext.py
@@ -15,10 +15,22 @@ if ftype[1] == 'aliraw':
 	file=open(ftype[0],'r')
 	file=file.read().strip().split('\n')
 	title=file[0]
-	descrip=file[2]
+	descrip=file[1]
 	descrip=descrip[1:]
 	print(descrip)
 	sleep(4)
+	file=file[2:]
 	if title == '<ALIAS>':
+		for line in file:
+			os.system('echo \"'+line+'\" >> ~/.bash_aliases')
 	elif title == '<COPY>':
+		file='\n'.join(file)
+		print('--------------COPIED:---------------')
+		print(file)
+		pyperclip.copy(file)
 	elif title == '<RUN>':
+		os.system('echo \"'+file+'\" > .tmp_runfile')
+		os.system('bash .tmp_runfile')
+		os.system('rm .tmp_runfile')
+elif ftype[1] == 'atxt':
+	#Do everything else
